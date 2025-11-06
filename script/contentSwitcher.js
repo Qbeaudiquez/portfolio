87ec9fba48bb443cb35831cddb1abb21e87914b6
Quentin Beaudiquez <q.beaudiquez.pro@gmail.com>
2025-11-06 18:36:59 +0100

    fix switcher

--- script/contentSwitcher.js
+++ script/contentSwitcher.js
@@ -1,20 +1,34 @@
 const links = document.querySelectorAll("a")
 
-links.forEach(link => {
-    link.addEventListener("click", (e) => {
-        e.preventDefault();
-        const pageContent = document.querySelector(".pageContent")
-        pageContent.innerHTML = ""
-        console.log("hello")
+    links.forEach(link => {
+        link.addEventListener("click", (e) => {
+            e.preventDefault();
 
-        const menuLinks = document.querySelector(".menuLinks")
-        const rings = document.querySelectorAll(".ring")
+            const menuLinks = document.querySelector(".menuLinks")
+            const rings = document.querySelectorAll(".ring")
 
-        rings.forEach(ring => {
-            ring.classList.remove("actived")
-        });
+            rings.forEach(ring => {
+                ring.classList.remove("actived")
+            });
 
-        menuLinks.classList.remove("actived")
-        })
+            menuLinks.classList.remove("actived")
+            page = link.getAttribute("data-page")
+            localStorage.setItem("currentPage", page);
+            loadPage(page)
+            console.log(page)
+            })
+
+        
+
+    });
+
+    window.addEventListener("load", () => {
+    const savedPage = localStorage.getItem("currentPage");
+
+    
+    if (savedPage) {
+        loadPage(savedPage);
+    } else {
+        loadPage("home");
+    }
 });
-
